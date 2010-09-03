84b570ca8ac692156fc146b6e4fb628f1b8b5378
Kjetil Kjernsmo <kjetil@startsiden.no>
2010-09-03 16:43:23 +0200

    first commit

---
+++ lib/Test/RDF.pm
@@ -0,0 +1,110 @@
+package Test::RDF;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Test::RDF - The great new Test::RDF!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::RDF;
+
+    my $foo = Test::RDF->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo, C<< <kjetilk at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-rdf at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-RDF>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::RDF
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-RDF>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-RDF>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-RDF>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test-RDF/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Kjetil Kjernsmo.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Test::RDF
---
+++ t/00-load.t
@@ -0,0 +1,10 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok( 'Test::RDF' ) || print "Bail out!
+";
+}
+
+diag( "Testing Test::RDF $Test::RDF::VERSION, Perl $], $^X" );
